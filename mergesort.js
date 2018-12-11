5773baf5d26b17fd868e6e0b13a299cb9ad0bd32
Dilan Ozusta <dilanozusta@outlook.com>
2018-12-11 00:21:03 -0600

    Change laptop push

--- mergesort.js
+++ mergesort.js
@@ -9,21 +9,33 @@ function split(wholeArray) {
 
   return [firstHalf, secondHalf];
 }
-function merge(arr1, arr2){
-  return arr1[0] < arr2[0] ? [arr1[0], arr2[0]] : [arr2[0], arr1[0]];
+function merge(left, right){
+  let mergedArr = [];
+  let leftIdx = 0;
+  let rightIdx = 0;
+  let leftVal;
+  let rightVal;
+  
+  while (leftIdx < left.length || rightIdx < right.length)
+    if ()
 }
 
 function mergeSort(arr) {
-  let temp = [];
+  let splitArr = split(arr);
+  let leftArr = splitArr[0];
+  let rightArr = splitArr[1];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].length === 1){
-      temp.push(arr[i]);
-    }
-    else {
-      split(arr[i]);
-    }
-  }
+  
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (arr[i].length === 1){
+  //     temp.push(arr[i]);
+  //   }
+  //   else {
+  //     split(arr[i]);
+  //   }
+  // }
 
   return temp;
 }
+
+console.log(mergeSort([0, 3, 3002, 5]));
